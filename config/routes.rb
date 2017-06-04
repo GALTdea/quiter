5384a4a0719ebda62ea6d5adc5e6591e490a2a13
GALTdea <gustavovalenzuela1180@gmail.com>
2017-06-03 18:12:22 -0700

    added more detailing to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/home' => 'pages#home' # override default routes.
   get '/user/:id' => 'pages#profile'
   get '/explore' => 'pages#explore'
-  get '/profile' => 'pages#profile'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
